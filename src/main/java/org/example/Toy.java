5fd607fad502e3f6b4ab520777a85bc6e8f4cad7
Vasily Levit <vasily.levit@gmail.com>
2023-03-13 21:59:58 +0800

    inserted incapsulation filds

--- src/main/java/org/example/Toy.java
+++ src/main/java/org/example/Toy.java
@@ -1,13 +1,33 @@
 package org.example;
 
+/**
+ * Класс Игрушка
+ */
 public class Toy {
-    int id;
-    int quantity;
-    int probability;
+    private int id;
     String name;
+    private int chance;
+    int quantity;
 
+    /**
+     *
+     * @param id идентификационный номер
+     * @param name название игрушки
+     * @param chance вероятность выигрыша
+     * @param quantity количество в магазине
+     */
+    public Toy(int id, String name, int chance, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.chance = chance;
+        this.quantity = quantity;
+    }
+
+    public int getChance() {
+        return chance;
+    }
     @Override
     public String toString() {
-        return String.format("id:%d name:%s probability:%s quantity:%d", id, name, probability, quantity );
+        return String.format("id:%d name:%s chance:%s quantity:%d", id, name, chance, quantity );
     }
 }
